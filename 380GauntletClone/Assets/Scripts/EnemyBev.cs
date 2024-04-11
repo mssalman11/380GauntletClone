94e94bfa57b7f502c01c857c0d7cf8645a740e2e
mssalman11 <mssalman@csuchico.edu>
2024-04-11 13:38:10 -0700

    VERY rudimentary spawn system
    need to make it spawn where I want it to

---
+++ 380GauntletClone/Assets/Scripts/EnemyBev.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBev : MonoBehaviour
+{
+    protected int enemyHealth;
+    protected int enemyLevel;
+    protected int enemyDamage;
+
+    public virtual void EnemyMove()
+    {
+
+    }
+
+}
---
+++ 380GauntletClone/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject prefabTest;
+    public GameObject prefabTestLVL2;
+    public GameObject prefabTestLVL3;
+    //public GameObject prefab2Test;
+
+    [SerializeField]
+    private int spLevel;
+
+    [SerializeField]
+    private bool spLevel1, spLevel2, spLevel3;
+
+    [SerializeField]
+    private bool isBone, isBlock;
+
+    private int spawnerHealth;
+
+    [SerializeField]
+    private bool playerEntered;
+
+    [SerializeField]
+    private bool isKilled;
+
+    private void Update()
+    {
+        if (playerEntered)
+        {
+            if (isBlock)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    spawnBlock(spLevel);
+                    isBlock = false;
+                }
+            }
+
+        }
+
+        if (isKilled)
+        {
+            DestroySpawner();
+        }
+    }
+
+
+    public void spawnBlock(int eLevel)
+    {
+        if (spLevel1)
+        {
+            Instantiate(prefabTest);
+        } else if (spLevel2){
+            Instantiate(prefabTestLVL2);
+        } else if (spLevel3)
+        {
+            Instantiate(prefabTestLVL3);
+        }
+
+
+    }
+
+    /*
+
+    public void spawnBone(int eLevel)
+    {
+        Instantiate(prefab2Test);
+    }
+    */
+
+    public void DestroySpawner()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+}
